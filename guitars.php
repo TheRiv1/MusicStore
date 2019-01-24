1737037e187b16a63c61c9981502c6768d91f469
TheRiv1 <j_spilo@hotmail.com>
2019-01-24 13:34:47 +0100

    update files

--- guitars.php
+++ guitars.php
@@ -32,22 +32,29 @@ include 'header.php';
     foreach ($db_result as $row)
     {            
         
-        echo '<div class="card2 debug">' .
-        			//Brand + Model
-            '<h1>' . $row['Guitar_Brand']  . ' ' . $row['Guitar_Model'] .'</h1>' .
-            
-            //Img 
-             '<img src="' . $row['Guitar_Img'] . '" alt="'  . '" style="width:30%">' .
-             //Price
-             '<h2 class="price">' .'€'. $row['Guitar_Price'] .',-'.'</h2>'.
-             //Art.nr
-             '<p>' . 'Articlenumber: ' .  $row['Guitar_Articlenumber'] . '</p>' . 
-             //Info
-             '<p>' . $row['Guitar_Info'] . '</p>' .
-             //button
-             '<a href="https://www.musicstore.de/nl_NL/EUR/cart">'. '<p>' . '<button class="btn2">'. 'Add to cart' . '</button>' . '</P>' .
-             '</a>' .
-            '</div>';
+        echo '<div class="card_single debug">' .
+		        			'<div class="left debug">' .
+		        				//Brand + Model
+		             		'<h1>' . $row['Guitar_Brand']  . ' ' . $row['Guitar_Model'] .'</h1>' . 
+
+		             			 //Price
+		             		'<h2 class="price">' .'€'. $row['Guitar_Price'] .',-'.'</h2>'.  
+
+		             		//Img 
+		             		'<img src="' . $row['Guitar_Img'] . '" alt="'  . '" style="width:100%">' .
+		            	
+		             		
+		             '</div>' .
+		             '<div class="right debug">' .
+				             //Art.nr
+				             '<p>' . 'Articlenumber: ' .  $row['Guitar_Articlenumber'] . '</p>' . 
+				             //Info
+				             '<p>' . $row['Guitar_Info'] . '</p>' .
+				             //button
+				             '<a href="https://www.musicstore.de/nl_NL/EUR/cart">'. '<p>' . '<button class="cart_btn">'. 'Add to cart' . '</button>' . '</P>' .
+				             '</a>' .
+		             '</div>' .
+             '</div>';
        
     }     
    
